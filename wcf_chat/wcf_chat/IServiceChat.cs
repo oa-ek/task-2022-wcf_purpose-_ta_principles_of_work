b2637cb1da016e0b13a3e16137b084b4ccb6a19d
Alexander Puchko <oleksandr.puchko@oa.edu.ua>
2022-12-13 09:57:07 +0200

    Program + Presentation

---
+++ wcf_chat/wcf_chat/IServiceChat.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace wcf_chat
+{
+    // ПРИМЕЧАНИЕ. Можно использовать команду "Переименовать" в меню "Рефакторинг", чтобы изменить имя интерфейса "IServiceChat" в коде и файле конфигурации.
+    [ServiceContract(CallbackContract = typeof(IServerChatCallback))]
+    public interface IServiceChat
+    {
+        [OperationContract]//Метод в якого є, атрибут "OperationContract" буде видний зі сторони клієнта, щоб ми потом використовували його клієнтом
+        int Connect(string name); //Метод для підключення до сервісу
+
+        [OperationContract]
+        void Disconnect(int id); //Метод для відключення з сервісу
+
+        [OperationContract(IsOneWay = true)]
+        void SendMsg(string mgs, int id); //Метод для приймання сповіщення, та вказування, від кого
+
+
+    }
+
+
+    public interface IServerChatCallback //Вертає повідомлення
+    {
+        [OperationContract(IsOneWay = true)]
+        void MsgCallback(string msg);
+    }
+}
---
+++ wcf_chat/wcf_chat/ServiceChat.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace wcf_chat
+{
+    // ПРИМЕЧАНИЕ. Команду "Переименовать" в меню "Рефакторинг" можно использовать для одновременного изменения имени класса "ServiceChat" в коде и файле конфигурации.
+    [ServiceBehavior(InstanceContextMode = InstanceContextMode.Single)] //Single в всіх общий сервіс 
+    public class ServiceChat : IServiceChat //Реалізуєм інтерфейс описаний в IServeceChat
+    {
+        List<ServerUser> users = new List<ServerUser>();
+        int nextId = 1;
+        public int Connect(string name)
+        {
+            ServerUser user = new ServerUser()
+            {
+                ID = nextId,
+                Name = name,
+                operationContext = OperationContext.Current
+            };
+            nextId++;
+
+            SendMsg(": "+user.Name+" Підключився до чату",0);
+            users.Add(user);
+            return user.ID;
+        }
+
+        public void Disconnect(int id)
+        {
+            var user = users.FirstOrDefault(i => i.ID == id);
+            if (user!=null)
+            {
+                users.Remove(user);
+                SendMsg(": "+user.Name + " закінчив сесію",0);
+            }
+        }
+
+        public void SendMsg(string msg, int id)
+        {
+            foreach(var item in users)
+            {
+                string answer = DateTime.Now.ToShortTimeString();
+
+                var user = users.FirstOrDefault(i => i.ID == id);
+                if (user != null)
+                {
+                    answer += ": " + user.Name + " ";
+                }
+                answer += msg;
+                item.operationContext.GetCallbackChannel<IServerChatCallback>().MsgCallback(answer);
+            }
+        }
+    }
+}
